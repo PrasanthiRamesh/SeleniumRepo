45c66b46756169bfa61324c3e90a4484b5902e3c
PrasanthiRamesh <114842679+PrasanthiRamesh@users.noreply.github.com>
2022-10-15 08:32:25 -0700

    Add files via upload

---
+++ Week3Asignment/AxisBank.java
@@ -0,0 +1,12 @@
+package Week3Asignment;
+
+public class AxisBank extends BankInfo {
+	
+	public void deposit() {
+		System.out.println("Axis bank class ");
+	}
+	public static void main(String[] args) {
+		AxisBank ab = new AxisBank();
+		ab.deposit();
+	}
+}
---
+++ Week3Asignment/BankInfo.java
@@ -0,0 +1,19 @@
+package Week3Asignment;
+
+public class BankInfo {
+
+	public void saving() {
+
+	}
+
+	public void fixed() {
+
+	}
+
+	public void deposit() {
+		
+		System.out.println("bank info class");
+
+	}
+
+}
